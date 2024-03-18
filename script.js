0cfe73222bc30339daf7ea55652ba20f2f8a701e
Gerile3 <28847330+Gerile3@users.noreply.github.com>
2024-03-18 21:49:19 +0300

    add converter logic

---
+++ script.js
@@ -0,0 +1,50 @@
+const cButton = document.getElementById("convert-btn")
+const result = document.getElementById("output")
+const convertTable = {
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
+    "IX": 9,
+    "V": 5,
+    "IV": 4,
+    "I": 1
+}
+
+const convertToRoman = () => {
+    result.textContent = ""
+    const number = document.getElementById("number")
+
+    if (number.value === ""){
+        result.textContent = "Please enter a valid number";
+        return
+    }
+
+    if (number.value > 3999){
+        result.textContent = "Please enter a number less than or equal to 3999";
+        return
+    }
+    if (number.value < 0){
+        result.textContent = "Please enter a number greater than or equal to 1";
+        return
+    }
+
+    let decimal = number.value
+
+    for (const [roman,numeral] of Object.entries(convertTable)) {
+        while(decimal >= numeral){
+            decimal -= numeral;
+            result.textContent += roman;
+        }
+    }
+}
+
+
+cButton.addEventListener("click", convertToRoman)
+
+
